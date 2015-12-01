13e6f2fb659dee9b8540487d72d0d224276ddfb4
resteve <resteve@zikzakmedia.com>
2015-12-01 11:02:01 +0100

    Tests

--- tests/test_account_payment_type_move.py
+++ tests/test_account_payment_type_move.py
@@ -1,36 +1,18 @@
-#!/usr/bin/env python
-# -*- coding: UTF-8 -*-
-# This file is part account_payment_type_move module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
+# This file is part of the account_payment_type_move module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
-import doctest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
-from trytond.tests.test_tryton import doctest_setup, doctest_teardown
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class AccountPaymentTypeMoveTestCase(unittest.TestCase):
-    'Test AccountPaymentTypeMove module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('account_payment_type_move')
-
-    def test0005views(self):
-        'Test views'
-        test_view('account_payment_type')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+class AccountPaymentTypeMoveTestCase(ModuleTestCase):
+    'Test Account Payment Type Move module'
+    module = 'account_payment_type_move'
 
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         AccountPaymentTypeMoveTestCase))
-    suite.addTests(doctest.DocFileSuite(
-            'scenario_payment_type_move.rst',
-            setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
-            optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
