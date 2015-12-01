8af8c08b7e4dd22e79ca5814a701677bcbc88692
resteve <resteve@zikzakmedia.com>
2015-12-01 11:42:50 +0100

    Tests Scenario

--- tests/test_account_payment_type_move.py
+++ tests/test_account_payment_type_move.py
@@ -2,8 +2,10 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 import unittest
+import doctest
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import ModuleTestCase
+from trytond.tests.test_tryton import doctest_setup, doctest_teardown
 
 
 class AccountPaymentTypeMoveTestCase(ModuleTestCase):
@@ -15,4 +17,8 @@ def suite():
     suite = trytond.tests.test_tryton.suite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         AccountPaymentTypeMoveTestCase))
+    suite.addTests(doctest.DocFileSuite(
+            'scenario_payment_type_move.rst',
+            setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
+            optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
